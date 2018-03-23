e4acf1bee36b3fb50aaef13f11ae15e9e7e58e0b
VinuAnand <reachvinunow@gmail.com>
2018-03-24 00:40:04 +0530

    created teacher bean in main of studentbean

--- ForGIT/src/mains/ForStudentBeanMain.java
+++ ForGIT/src/mains/ForStudentBeanMain.java
@@ -1,12 +1,14 @@
 package mains;
 
 import beans.StudentBean;
+import beans.TeacherBean;
 
 public class ForStudentBeanMain {
 
 	public static void main(String[] args) {
 		StudentBean bean = new StudentBean();
 		System.out.println("Student Name : "+bean.getStudentName());
+		TeacherBean teacherBean = new TeacherBean();
 	}
 
 }
